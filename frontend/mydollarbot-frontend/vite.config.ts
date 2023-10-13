ccb637b783cb41266b5f7de4571896d3fccf7155
Harris-A-Khan <harris.a.khan2003@gmail.com>
2023-10-13 16:55:28 -0400

    feat: allow cors

--- frontend/mydollarbot-frontend/vite.config.ts
+++ frontend/mydollarbot-frontend/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000', // replace with your backend server URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // if you want to remove /api prefix
+      }
+    }
+  }
 })
