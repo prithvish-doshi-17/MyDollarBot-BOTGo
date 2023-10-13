f209b9a91b66cfb3736faca57a03f9b27d1bff16
Harris-A-Khan <harris.a.khan2003@gmail.com>
2023-10-13 17:13:58 -0400

    feat: create main landing page with dummy data, ready for team development

--- backend/src/api.py
+++ backend/src/api.py
@@ -12,13 +12,39 @@ def landing():
 
 @app.route('/dummyData')
 def data(): 
-    # Sample data for testing
-    data =  [
+    # Budget data
+    budget_data = [
         {"category": "Food", "allocated": 500, "spent": 300, "remaining": 200},
         {"category": "Rent", "allocated": 1000, "spent": 1000, "remaining": 0},
-        {"category": "Entertainment", "allocated": 100, "spent": 50, "remaining": 50}
+        {"category": "Entertainment", "allocated": 100, "spent": 50, "remaining": 50},
+        # ... Add other categories as needed
     ]
-    return json.dumps(data)
+
+    # Individual spending data
+    spendings = [
+        {"category": "Food", "amount": 50, "description": "Dinner at ABC restaurant", "date": "2023-10-01"},
+        {"category": "Food", "amount": 30, "description": "Groceries", "date": "2023-10-02"},
+        {"category": "Entertainment", "amount": 50, "description": "Movie tickets", "date": "2023-10-03"},
+        # ... Add other spendings as needed
+    ]
+
+    # Categories
+    categories = [
+        {"id": 1, "name": "Food"},
+        {"id": 2, "name": "Rent"},
+        {"id": 3, "name": "Entertainment"},
+        # ... Add other categories as needed
+    ]
+
+    # Combine all data into one object
+    data = {
+        "budget_data": budget_data,
+        "spendings": spendings,
+        "categories": categories,
+        # ... Add other data structures as needed
+    }
+
+    return jsonify(data)
 
 
 @app.route('/budgets')
